d779e391802dfbdbce4721cf86cc3d29e9f4ec15
ThisAngryGuy <mattgold127@gmail.com>
2018-10-08 17:49:02 -0400

    Clase - 08/10/18 Seugimiento de Camara y nivel

---
+++ CorredorOndaVital/Assets/Scripts/CamaraS.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraS : MonoBehaviour
+{
+    public GameObject Jugador;
+	void Update ()
+    {
+        transform.position = new Vector3 (Jugador.transform.position.x, 0, Jugador.transform.position.z - 10);
+	}
+}
