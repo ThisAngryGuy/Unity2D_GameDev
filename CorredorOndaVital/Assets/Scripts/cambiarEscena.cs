8d29d11ef8219a758d5ac3bbe34c5ae010c13a87
ThisAngryGuy <mattgold127@gmail.com>
2018-10-15 17:27:44 -0400

    Practica Menus para el juego 2D

---
+++ CorredorOndaVital/Assets/Scripts/cambiarEscena.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cambiarEscena : MonoBehaviour {
+    public void IrJuego()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void IrMenu()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void IrGameOver()
+    {
+        SceneManager.LoadScene(1);
+    }   
+}
+
---
+++ CorredorOndaVital/Assets/Scripts/dstruir.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class dstruir : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collisionador)
+    {
+        if(collisionador.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            if(collisionador.gameObject.transform.parent)
+            {
+                Destroy(collisionador.gameObject.gameObject.transform.parent);
+            }
+            else
+            {
+                Destroy(collisionador.gameObject);
+            }
+        }
+    }
+}
